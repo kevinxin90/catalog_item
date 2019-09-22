19d11767e61ae9ea2a70622c7aa9b47ac0830cd7
jiwen xin <kevinxin@scripps.edu>
2019-09-22 09:55:42 -0700

    add session type

--- views.py
+++ views.py
@@ -502,5 +502,6 @@ def deleteItem(item_id):
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
+    app.config['SESSION_TYPE'] = 'filesystem'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
